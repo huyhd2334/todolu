60d9a4e0b39401c1e821a1e47a90b472a479a1c9
huyhd2334 <huyqame1356@gmail.com>
2025-09-15 17:12:53 +0700

    deploy

--- BackEnd/scr/server.js
+++ BackEnd/scr/server.js
@@ -3,16 +3,24 @@ import taskRoute from './routes/taksRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
-
+const __dirname = path.resolve() //end project
 const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173" }))
+if (process.env.NODE_ENV !== "production") {app.use(cors({origin: "http://localhost:5173" }))}
 
 app.use("/api/tasks",taskRoute);
+
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*",(rep, res) => {res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))})
+}
 
 connectDB().then(()=>{
     app.listen(5001, () => {
